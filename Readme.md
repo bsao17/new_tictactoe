db3a6c78780566c25dafeb23920aff532c3c180d
bsao17 <declic62@gmail.com>
2023-11-07 19:12:44 +0100

    fix: button order in new tictactoe_ui.ui and tictactoe_ui.py

--- Readme.md
+++ Readme.md
@@ -5,4 +5,4 @@ here is my new version of the game Tic Tac Toe, (Morpion in French)
 But I'm including new rules so it will be a game between Go and Tic Tac Toe
 
 ## UI first view
-![ui picture](assets/tictactoe2.png)
+![ui picture](assets/tictactoe_ui.png)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from tictactoe_gui import Ui_MainWindow
+from tictactoe_ui import Ui_MainWindow
 
 from PyQt5 import QtWidgets
 
@@ -10,11 +10,12 @@ os.environ['QT_QPA_PLATFORM'] = 'wayland'
 class MyApplication(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MyApplication, self).__init__()
-        self.setupUi(self)  # Initialisation de l'interface utilisateur
+        self.setupUi(self)  # Initialisation de l'interface Ui_MainWindow
 
 
-app = QtWidgets.QApplication(sys.argv)
-window = MyApplication()
-window.show()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApplication()
+    window.show()
 
-sys.exit(app.exec_())
+    app.exec_()
