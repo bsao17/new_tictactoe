4c17b0644aba2f4119e7a4b16a86a1cef742ec78
bsao17 <declic62@gmail.com>
2023-11-08 17:24:42 +0100

    feat: update global design from QtDesigner

--- players/players_class.py
+++ players/players_class.py
@@ -8,3 +8,15 @@ class Players_class:
 
     def set_player_username(self, username):
         self.username = username
+
+    def get_player_label(self):
+        print(self.label)
+
+    def set_player_label(self, new_label):
+        self.label = new_label
+
+    def update_profile(self):
+        print("Choississez votre nom d'utilisateur et le symbole qui sera afficher dans vos cases.")
+        print()
+        self.get_player_username()
+        self.set_player_username()
