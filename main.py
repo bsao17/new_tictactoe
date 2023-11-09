db548e095deee419897783348945f432c7a98fa0
bsao17 <declic62@gmail.com>
2023-11-09 10:29:16 +0100

    feat: adding close event slot on push button "stop"

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from game_core.game_functions import messages
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import pyqtSlot
 from tictactoe_ui import Ui_MainWindow
 import sys
 from players.players import Players
@@ -12,6 +13,11 @@ class Tictactoe_main(QtWidgets.QMainWindow, Ui_MainWindow):
         super(Tictactoe_main, self).__init__()
         # Initialisation de l'interface Ui_MainWindow
         self.setupUi(self)
+        self.pushButton_stop.clicked.connect(self.on_close_triggered)
+
+    @pyqtSlot()
+    def on_close_triggered(self):
+        self.close()
 
 
 if __name__ == '__main__':
