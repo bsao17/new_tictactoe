b5ad7a15f9acee7c35050b7478aaaf75cd93f833
Bruno M <declic62@gmail.com>
2023-11-24 18:59:45 +0100

    feat: implement messages test

--- game_core/game_functions.py
+++ game_core/game_functions.py
@@ -1,9 +1,13 @@
 from PyQt5.QtWidgets import QMessageBox
 
 
-def messages(title, message):
-    """
-    Displays an information message box with the given title and message.
-    """
-    # Return QMessageBox.information(self, title, message, QMessageBox.Ok)
-    print(title, message)
+class Messages:
+    def __init__(self, title, message):
+        self.title = title
+        self.message = message
+
+    def information_message(self):
+        """
+        Displays an information message box with the given title and message.
+        """
+        QMessageBox.information(self.title, self.message)
