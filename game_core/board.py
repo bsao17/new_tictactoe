0cba64f8ac301e6b8fdb1d12a45366f147f61fba
Bruno M <declic62@gmail.com>
2023-11-27 19:09:47 +0100

    feat: delete children function

--- game_core/board.py
+++ game_core/board.py
@@ -1,6 +1,3 @@
-import copy as cp
-
-
 class Board:
     def __init__(self):
         # Initialise the magic_id, the sum of each alignment of four must equal 34
@@ -9,16 +6,14 @@ class Board:
                          [9, 6, 7, 12],
                          [4, 15, 14, 1]]
         self.board = [[None, None, None, None],
-                      ["X", "X", "X", "X"],
                       [None, None, None, None],
-                      [None, "O", None, None], ]
+                      [None, None, None, None],
+                      [None, None, None, None], ]
         self.x_valid_align = []
         self.o_valid_align = []
 
-    # Align list of valid lists
+    # Check valid alignement for X or O
     def check_valid_alignment(self):
-        x_valid_alignement = []
-        o_valid_alignement = []
         for row in range(4):
             for col in range(4):
                 if self.board[row][col] == "X":
@@ -32,28 +27,6 @@ class Board:
         x_win = print("X Winner") if sum(self.x_valid_align) == 34 else print("X Loser")
         o_win = print("O Winner") if sum(self.o_valid_align) == 34 else print("O Loser")
 
-    def children_score(self, player):
-        children = []
-        if player == "X":
-            for i in range(4):
-                for j in range(4):
-                    if self.board[i][j] == 0:
-                        child = Board()
-                        child.board = [x[:] for x in self.board]
-                        child.board[i][j] = 1
-                        children.append(child)
-        else:
-            for i in range(4):
-                for j in range(4):
-                    if self.board[i][j] == 0:
-                        child = Board()
-                        child.board = [x[:] for x in self.board]
-                        child.board[i][j] = 2
-                        children.append(child)
-        return children
-
 
 if __name__ == '__main__':
-    my_board = Board()
-    my_board.check_valid_alignment()
-    my_board.check_win()
+    pass
