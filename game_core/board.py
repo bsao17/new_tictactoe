58e154d8d22293d35d6d156c3422d9a47cc479f2
Bruno M <declic62@gmail.com>
2023-11-28 12:26:09 +0100

    feat: add diagonal

--- game_core/board.py
+++ game_core/board.py
@@ -8,12 +8,20 @@ class Board:
                                   [5, 10, 11, 8],
                                   [9, 6, 7, 12],
                                   [4, 15, 14, 1]])
-        self.board = np.array([[1, None, None, None],
-                               [2, None, None, None],
-                               [3, None, None, None],
-                               [4, None, None, None], ])
-        self.row_1 = self.board[: 1, : 4]
-        self.row_2 = self.board[1 : 2, 0 : 4]
+        self.board = np.array([[1, None, None, 2],
+                               [None, None, None, None],
+                               [None, None, None, None],
+                               [3, None, None, 4]])
+        self.row_1 = self.board[0]
+        self.row_2 = self.board[1]
+        self.row_3 = self.board[2]
+        self.row_4 = self.board[3]
+        self.col_1 = self.board[:, 0]
+        self.col_2 = self.board[:, 1]
+        self.col_3 = self.board[:, 2]
+        self.col_4 = self.board[:, 3]
+        self.diag_1 = np.diag(self.board)
+        self.diag_2 = np.diag(np.fliplr(self.board))
         self.x_valid_align = []
         self.o_valid_align = []
 
@@ -34,4 +42,13 @@ class Board:
 
 if __name__ == '__main__':
     my_board = Board()
-    print(my_board.row_2)
+    print("row 1", my_board.row_1)
+    print("row 2", my_board.row_2)
+    print("row 3", my_board.row_3)
+    print("row 3", my_board.row_4)
+    print("col 1", my_board.col_1)
+    print("col 2", my_board.col_2)
+    print("col 3", my_board.col_3)
+    print("col 4", my_board.col_4)
+    print("diag 1 ", my_board.diag_1)
+    print("diag 2 ", my_board.diag_2)
