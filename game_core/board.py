8da93e034d51640fd3af5026dc1851347b3a3d2f
Bruno M <declic62@gmail.com>
2023-11-30 13:45:42 +0100

    feat: valid alignment check ok

--- game_core/board.py
+++ game_core/board.py
@@ -20,22 +20,50 @@ class Board:
         self.diag_2 = np.diag(np.fliplr(self.board))
         self.diag_3 = np.flip(self.diag_1, 0)
         self.diag_4 = np.flip(self.diag_2, 0)
+
+        self.move_player = np.array(self.board)
+
+        self.valid_align_list = [
+            self.row_1,
+            self.row_2,
+            self.row_3,
+            self.row_4,
+            self.col_1,
+            self.col_2,
+            self.col_3,
+            self.col_4,
+            self.diag_1,
+            self.diag_2,
+            self.diag_3,
+            self.diag_4,
+        ]
+
         self.x_valid_align = []
         self.o_valid_align = []
 
     # Check valid alignement for X or O
     def check_valid_alignment(self):
-        for row in range(1, 5):
-            for col in range(1, 5):
-                print(f"row: {row}, column: {col}")
+        # rows check
+        for row in self.board:
+            if self.is_valid_align(row):
+                return True
+
+        # columns check
+        for col in self.board.T:  # (numpy method) T -> Transpose or turn the board for rows to cols and cols to rows
+            if self.is_valid_align(col):
+                return True
+
+        # diagonales check
+        if self.is_valid_align(np.diag(self.board)) or \
+                self.is_valid_align(np.diag(np.fliplr(self.board))):
+            return True
+
+        return False
 
     def is_valid_align(self, line):
-        if "x" in line:
-            return True
-        else:
-            False
+        return None not in line and len(set(line)) == 1
 
 
 if __name__ == '__main__':
     my_board = Board()
-    my_board.check_valid_alignment()
+    print(my_board.check_valid_alignment())
